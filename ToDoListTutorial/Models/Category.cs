49fd858781c45c8e0bee101426d1d5eee37a673a
Frank Eslami <eslamifrank@gmail.com>
2020-10-25 23:12:13 -0700

    Models created.

---
+++ ToDoListTutorial/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoListTutorial.Models {
+    public class Category {
+        public string CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ToDoListTutorial/Models/Status.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoListTutorial.Models {
+    public class Status {
+        public string StatusId { get; set; }
+        public string Name { get; set; }
+    }
+}
